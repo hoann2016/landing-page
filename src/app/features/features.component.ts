ed1bb0c9851656b917e160a9c4fa6dc8f5930ed5
dinhlam <lamvietdinhqn2204@gmail.com>
2019-11-18 09:15:09 +0700

    [LB-104]-Fix popup step3

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -1,16 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { LandingPageService } from '../shared/services/landing-page.service';
 import {Router} from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-features',
   templateUrl: './features.component.html',
-  styleUrls: ['./features.component.scss']
+  styleUrls: ['./features.component.scss'],
+  encapsulation: ViewEncapsulation.None
+
 })
 export class FeaturesComponent implements OnInit {
-  constructor() {
+  currentDemoStep: number;
+  constructor(private modalService: NgbModal) {
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.currentDemoStep = 3;
+  }
 
+  openDemoPopup(content) {
+    this.modalService.open(content, { centered: true, windowClass: 'demo-booking-popup' });
+  }
 }
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FeaturesRoutes } from './features.routing';
 import { FeaturesComponent } from './features.component';
 import { TranslateModule } from '@ngx-translate/core';
+import {NgbAccordionModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [FeaturesComponent],
   imports: [
     CommonModule,
     FeaturesRoutes,
+    NgbAccordionModule,
     TranslateModule
   ]
 })
