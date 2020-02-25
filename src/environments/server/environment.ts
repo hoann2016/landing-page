d126caec742bc9da6d0b7ddb4c2f6766d1c6629e
Clement <anh.le@ludiino.com>
2020-02-25 20:08:58 +0700

    [LB-197] Add dev env

---
+++ src/environments/server/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    isServer: true,
+    host: 'http://localhost:4000',
+    rootproto:"http://",
+    rootip:"dev.ludiino.com",
+    rootpath:"/",
+    socketPath:'/socket',
+}
