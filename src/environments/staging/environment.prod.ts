6156f8e4d93c7614b32033d32edbcaf3cd8438a4
Clement <anh.le@ludiino.com>
2020-02-25 15:22:15 +0700

    [LB-197] Create env, command for staging

---
+++ src/environments/staging/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    isServer: true,
+    rootproto:"http://",
+    rootip:"staging.ludiino.com",
+    rootpath:"/",
+    port:null,
+    socketPath:'/socket',
+    socketPort:null,
+    merchantpath:"http://45.77.250.47:84",
+    redirectTime:15
+  };
