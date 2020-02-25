e07c0d4671bbea228ae18ce40af952b49dfeda2b
Clement <anh.le@ludiino.com>
2020-02-25 20:21:14 +0700

    [LB-197] Add env for dev

--- src/environments/server/environment.ts
+++ src/environments/server/environment.ts
@@ -1,9 +1,30 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 export const environment = {
     production: false,
-    isServer: true,
-    host: 'http://localhost:4000',
+    isServer: false,
+    
+    // for prerender
+  
+    // host: 'http://localhost:4000',
+    // rootproto:"http://",
+    // rootip:"localhost",
+    // rootpath:"/",
+    // socketPath:'/socket',
+    // socketPort:9992,
+    // port:8080,
+    // merchantpath:"http://45.77.250.47:84",
+    // redirectTime:15
+  
     rootproto:"http://",
     rootip:"dev.ludiino.com",
     rootpath:"/",
     socketPath:'/socket',
-}
+    socketPort:null,
+    port:null,
+    merchantpath:"http://45.77.250.47:84",
+    redirectTime:15
+  };
